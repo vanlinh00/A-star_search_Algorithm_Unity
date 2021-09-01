b168a31ee31506b1a8560d1846c25a34b71a204c
vanlinh00 <82046592+vanlinh00@users.noreply.github.com>
2021-09-01 17:39:26 +0700

    Create Project

---
+++ A star Search Algorithm/Assets/Scripts/Block.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+   
+    private bool isSelected = false;
+
+    private SpriteRenderer render;
+    void Awake()
+    {
+      
+
+    }
+    private void Select()
+    {
+        isSelected = true;
+      
+        
+    }
+
+    private void Deselect()
+    {
+        isSelected = false;
+       
+    }
+
+	// Start is called before the first frame update
+	void OnMouseDown()
+	{
+	    if(isSelected==false)
+        {
+            Select();
+            Debug.Log("x,y"+transform.position.x+transform.position.y);
+        }    
+	}
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ A star Search Algorithm/Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+    public   List<Block> Openlist;
+    public  List<Block> CloseList;
+
+    void Start()
+    {
+        Instance = this;
+    }
+
+    public void DistanceAandB()
+    {
+        //Openlist[1].transform.position.x;
+       // Debug.LogError(Openlist[1].transform.position.x);
+      //   float a=Mathf.Sqrt(Openlist[1].transform.position.x-)
+    }    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
